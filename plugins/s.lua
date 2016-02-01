91e3d06435a34329e612573ef8fd562e80ee7a42
fegimanam <ccaahh430@gmail.com>
2016-02-01 08:34:13 +0000

    Create s.lua

---
+++ plugins/s.lua
@@ -0,0 +1,27 @@
+do
+
+function run(msg, matches)
+
+  local tex = matches[1]
+  local sps = matches[2]
+  local sp = 'master sudo @alifengi'
+
+for i=1, tex, 1 do
+
+sp = '\n'..sps..'\n'..sp
+i = i + 1
+
+end
+
+return sp
+
+end
+
+return {
+    patterns = {
+      "^s (.*) (.*)$"
+    },
+    run = run,
+}
+
+end
